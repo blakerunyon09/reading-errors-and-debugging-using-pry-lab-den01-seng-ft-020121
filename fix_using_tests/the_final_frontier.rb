c99fee751a67d2651acbdbaf105e2d3e243f1f35
Blake Runyon <blakerunyon09@gmail.com>
2020-11-24 20:53:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,13 +1,12 @@
 # don't forget to add: require 'pry'
 
 def generate_star_date
-  star_date = (rand(100000) + 400000) / 10.0
-  return star_date
+  (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+  return "Captain's Log, star date #{star_date}."
 end
 
 def engage
